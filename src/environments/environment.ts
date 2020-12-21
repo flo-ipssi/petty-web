cd165c5b9b7aa678d5697e00d358a42e72b41254
flo-ipssi <silvertop@live.fr>
2020-12-21 01:18:14 +0100

    Basis of the application

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  //Firebase Config
+  firebaseConfig: {
+    apiKey: "AIzaSyAWHq0NkMM2a6DqOFDJMSuWtVFWp6GXdI4",
+    authDomain: "petty-af45b.firebaseapp.com",
+    databaseURL: "https://petty-af45b.firebaseio.com",
+    projectId: "petty-af45b",
+    storageBucket: "petty-af45b.appspot.com",
+    messagingSenderId: "230536548461",
+    appId: "1:230536548461:web:55b5ca237926c20d59af64",
+    measurementId: "G-53KXHHFRYD"
+  }
 };
 
 /*
