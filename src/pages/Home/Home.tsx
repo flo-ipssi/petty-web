abd5e88d075b01f4eeda0f42bb12052abdd3be39
flo-ipssi <florian.emmanuel@outlook.fr>
2023-12-14 21:07:09 +0100

    Add Home Page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+interface HomeProps { }
+
+const Home: FC<HomeProps> = () => {
+
+  return (
+    <div className="max-w-3xl mx-auto space-y-6">
+      <div className="space-y-6 bg-white shadow-md rounded p-5">
+        OKOKOKO
+      </div>
+    </div>
+  )
+};
+
+export default Home;
